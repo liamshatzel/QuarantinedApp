abe2037eab1ffd7719b42ce8b38933535857e381
Liam Shatzel <liam.shatzel@icloud.com>
2020-04-04 20:31:18 -0700

    Initial Commit

---
+++ bored/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bored
+//
+//  Created by Liam Shatzel on 4/4/20.
+//  Copyright © 2020 Liam Shatzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
